72f9d7b91a3ee4106ddbc9faac2ceeaceff88f50
Byron <3018080310101@ingenieria.usac.edu.gt>
2023-01-04 02:50:12 -0600

    add frontend components

---
+++ MamuroEmail/src/components/CardComponent.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import CardItemComponent from "./CardItemComponent.vue";
+const items = [
+  {
+    title: "Allen-p",
+    content: "Message from to the future...",
+    file: "allen-p.png",
+    root: "allen-p",
+    date: "08/10/2022",
+  },
+  {
+    title: "Al2len-p",
+    content: "Message from to the future...",
+    file: "alle2n-p.png",
+    root: "al2len-p",
+    date: "082/10/2022",
+  },
+];
+</script>
+<template>
+  <ul class="flex flex-row gap-3 flex-wrap ml-7 mr-7 justify-center mb-10">
+    <li v-for="item in items" :key="item.title">
+      <CardItemComponent
+        :title="item.title"
+        :content="item.content"
+        :root="item.root"
+        :file="item.file"
+        :date="item.date"
+      />
+    </li>
+  </ul>
+</template>
---
+++ MamuroEmail/src/components/CardItemComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+defineProps<{
+  title: string;
+  content: string;
+  file: string;
+  root: string;
+  date: string;
+}>();
+</script>
+<template>
+  <article class="bg-white shadow-2xl rounded-2xl p-5 w-auto max-w-sm">
+    <h1 class="font-bold text-yellow-500">{{ title }}</h1>
+    <h3 class="font-bold text-yellow-200">{{ file }}</h3>
+    <p class="font-light text-gray-500 hover:font-bold">
+      {{ content.substring(0, 100) }}
+    </p>
+    <h6 class="text-sm text-gray-300 mb-5">Encontrado: {{ date }}</h6>
+    <a
+      href="#"
+      class="rounded-lg py-2 px-4 text-center text-white bg-yellow-400 hover:bg-yellow-500"
+      >Ver Mas...</a
+    >
+  </article>
+</template>
